5981007ea574bf426e1c9909c21cc801e33e1a90
andy <andy@lq-MacBook-Pro.local>
2025-11-10 22:39:30 +0300

    refactor main flow

--- index.rb
+++ index.rb
@@ -1,3 +1,12 @@
 require_relative './manager'
 
-Manager.new.start_game
+def start_game
+  result = Manager.new.start_game
+
+  if result && result[:new_game]
+    puts "******************* New Game *******************"
+    start_game
+  end
+end
+
+start_game
